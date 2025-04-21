09bfc207cba0b6cb8b6e6545e3b0915f357e9ac2
Daryl Brewer <daryl@zurigroup.com>
2025-04-21 10:34:43 -0400

    Changed loading ball color to #ee3524 for SPCA

--- src/app/utils/donation-multistep.js
+++ src/app/utils/donation-multistep.js
@@ -16,7 +16,7 @@ export class DonationMultistep {
       height: "",
       border_radius: "0",
       loading_color: "#E5E6E8",
-      bounce_color: "#16233f",
+      bounce_color: "#ee3524",
       append_url_params: "false",
     };
     this.donationinfo = {};
